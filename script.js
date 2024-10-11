0f4b6d60735283cb7225a53e9fb36061088704ee
Matheus Cantiere <matheus.cantieree@gmail.com>
2024-10-10 23:52:33 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Função de validação simples para o formulário de contato
 document.querySelector('form').addEventListener('submit', function(event) {
     alert('Formulário enviado com sucesso!');
     event.preventDefault();
